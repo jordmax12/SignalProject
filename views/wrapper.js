6ea17bdbd98dd90390e8a4545ea81c650a82c18b
jordmax12 <jordmax12@gmail.com>
2019-06-22 11:54:32 -0500

    updating page title and package json name

--- views/wrapper.js
+++ views/wrapper.js
@@ -1,7 +1,7 @@
 const wrapper = `<html>
 <head>
     <meta charSet="utf-8" />
-    <title>{Project Name Here}</title>
+    <title>Signal Coding Challenge</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
@@ -13,4 +13,4 @@ const wrapper = `<html>
 <script src="/main.js"></script>
 </html>`
 
-module.exports = {wrapper}
+module.exports = { wrapper }
