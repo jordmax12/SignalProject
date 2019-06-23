3cb5db9ac680137838661f46b45453e4257f7e90
jordmax12 <jordmax12@gmail.com>
2019-06-22 22:16:51 -0500

    api fix ?

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static(__dirname + '/views/assets'));
 app.use(express.static(__dirname + '/views/assets/css'));
 // Here we can do user validation middleware before accessing anything.
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('*', middleware)
 app.use(api);
 app.use('/', routes);
