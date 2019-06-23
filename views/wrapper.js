4c39d454cb395a31cfa324d5d6f7f67d07bc716f
jordmax12 <jordmax12@gmail.com>
2019-06-23 10:46:43 -0500

    make responsive for mobile

--- views/wrapper.js
+++ views/wrapper.js
@@ -2,6 +2,7 @@ const wrapper = `<html>
 <head>
     <meta charSet="utf-8" />
     <title>Signal Coding Challenge</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
